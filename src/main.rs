fe165e8a0163d0097b1b527146e23e9c17ca1ef7
Nonpawit Pothinil <nonpawit.p@outlook.com>
2024-07-24 21:13:05 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::io;
+
+fn main() {
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Failed to read line.");
+
+    println!("{:?}", user_input);
+}
